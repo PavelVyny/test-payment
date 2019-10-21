3ad3f6f91ff362400101ccbe492917d01e920357
PavelVyny <pavel.vynych@gmail.comgit config --global user.email pavel.vynych@gmail.comgit config --global user.email pavel.vynych@gmail.com>
2019-10-21 04:19:40 +0300

    -full adaptive added; -timer added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,64 @@
 $(document).ready(function(){
+	slickSlider()
+	paymentButtons()
+	timer()
+});
+
+function slickSlider() {
 	$('.slick-slider').slick({
 		infinite: true,
 		slidesToShow: 2,
 		dots: true,
+		autoplay: true,
+  		autoplaySpeed: 5000,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+			}
+		},
+		]
+
+
 	})
-});
+}
+
+function paymentButtons() {
+	var trialButton = $('.but-trial');
+	var annualButton = $('.but-annual');
+	var trialBlock = $('.block-trial');
+	var annualBlock = $('.block-annual');
+	var toOffers = $('.prop-to-offers-but');
+	var allBlocks = $('.set-block-prop');
+	var paymentForm = $('.offers-pay');
+	var offersContainer = $('.offers-set');
+	var headerText = $('.header-text');
+
+	trialButton.on("click", function() {
+		trialBlock.addClass('selected');
+		annualBlock.addClass('hide');
+		paymentForm.addClass('show');
+		offersContainer.addClass('column');
+		headerText.hide();
+
+	});
+
+	annualButton.on("click", function() {
+		annualBlock.addClass('selected');
+		trialBlock.addClass('hide');
+		paymentForm.addClass('show');
+		offersContainer.addClass('column');
+		headerText.hide();
+	});
+	toOffers.on("click", function() {
+		allBlocks.removeClass('hide selected');
+		paymentForm.removeClass('show');
+		offersContainer.removeClass('column');
+		headerText.show();
+	});
+}
+function timer() {
+	var newYear = new Date(); 
+	newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1);
+	$('.limited-offer-timer').countdown({until: newYear, format: 'dHM'});
+}
